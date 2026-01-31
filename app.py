8c947cbc5b540e091678ce2c002f89e7810da913
lalitaandashish <lalitaandashish@gmail.com>
2026-01-31 19:06:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, session
 import sqlite3, os
 from datetime import datetime
@@ -135,4 +136,10 @@ def logout():
     session.clear()
     return redirect("/")
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
+
+
